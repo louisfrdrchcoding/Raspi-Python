609f8de76a2c91ccfcd78b17615d9d72189d2314
Louis Friedrich <48963330+louisfrdrchcoding@users.noreply.github.com>
2019-09-22 12:58:21 +0200

    Update Speedtest.py

--- Speedtest.py
+++ Speedtest.py
@@ -1,5 +1,8 @@
 # how to print Speedtest results on Adafruit 128x32 display
 
+# first download:
+   # pip install speedtest-cli 
+
 
 
 
@@ -18,7 +21,7 @@ s.download(threads=threads)
 s.upload(threads=threads)
 s.results.share()
 
-result_dict = s.results.dict()
+result_dict = s.results.dict()      # list with Speedtest results in variable result_dict
 
 
 import time
@@ -30,10 +33,10 @@ from PIL import ImageFont
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
 disp.begin()
-disp.clear()
+disp.clear()                   # preparing display for usage
 disp.display()
 
-image = Image.new('1', (128, 32))
+image = Image.new('1', (128, 32))  # new image
 
 draw = ImageDraw.Draw(image)
 
@@ -44,16 +47,21 @@ result = result_dict['ping']         # getting Ping results from the list 'resul
 
 # download_result = result_dict['download']      # for getting download result
 # upload_result = result_dict['upload']          # for getting upload result
-res = '%d' % (result)                # changing Ping in decimal 
 
+res = '%d' % (result)                  # changing Ping in decimal 
+
+# download = '%d' % (download_result)  # changing download in decimal
+# upload = '%d' % (upload_result)      # changing upload in decimal
 
 
 while True:
-  
+# for printing download and upload results
+# draw.text((17, 3), "Upload:" + upload, font=fontN, fill=255)
+# draw.text(17, 6), "Download:" + download, font=fontN, fill=255)
   draw.text((17, 3), "Ping: " + res, font=fontN, fill=500)
   disp.image(image)
   disp.display()                     # printing Ping 
-  time.sleep(3)                      # update Ping result every 3 Seconds
+  time.sleep(3)                      # updating Ping result every 3 Seconds
   disp.clear()
   disp.display()
 
