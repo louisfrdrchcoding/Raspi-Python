7243545a229e70600759bf0a0cd9088b10a90bd1
Louis Friedrich <48963330+louisfrdrchcoding@users.noreply.github.com>
2019-09-22 12:46:34 +0200

    Update Speedtest.py

--- Speedtest.py
+++ Speedtest.py
@@ -1,13 +1,19 @@
+# how to print Speedtest results on Adafruit 128x32 display
+
+
+
+
 import speedtest
-import time
-import Adafruit_SSD1306
-servers = []
+import time                 # importing modules
+import Adafruit_SSD1306   
+
+servers = []             
 
 threads = None
 
-s = speedtest.Speedtest()
+s = speedtest.Speedtest()     
 s.get_servers(servers)
-s.get_best_server()
+s.get_best_server()            # making Speedtest
 s.download(threads=threads)
 s.upload(threads=threads)
 s.results.share()
@@ -15,40 +21,37 @@ s.results.share()
 result_dict = s.results.dict()
 
 
-
 import time
-from PIL import Image
-from PIL import ImageDraw
+from PIL import Image                  
+from PIL import ImageDraw      # importing display modules
 from PIL import ImageFont
 
-RST = 24
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
+
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
 disp.begin()
 disp.clear()
 disp.display()
 
-width = disp.width
-heigth = disp.height
-
-image = Image.new('1', (width, heigth))
+image = Image.new('1', (128, 32))
 
 draw = ImageDraw.Draw(image)
 
 font = ImageFont.load_default()
 fontN = ImageFont.truetype('FreeSans.ttf', 30)
-result = result_dict['ping']
-res = '%d' % (result)
+
+result = result_dict['ping']         # getting Ping results from the list 'result_dict'
+
+res = '%d' % (result)                # changing Ping in decimal 
+
+
 
 while True:
-
+  
   draw.text((17, 3), "Ping: " + res, font=fontN, fill=500)
   disp.image(image)
-  disp.display()
-  time.sleep(3)
+  disp.display()                     # printing Ping 
+  time.sleep(3)                      # update Ping result every 3 Seconds
   disp.clear()
   disp.display()
 
